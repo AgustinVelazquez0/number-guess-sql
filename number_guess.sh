2ec829ea00aa684d4d8f7de99d123dd630aaa542
Agustin Velazquez <agubolso2@gmail.com>
2025-04-27 02:10:04 +0000

    feat: Added number guessing game and input validation

--- number_guess.sh
+++ number_guess.sh
@@ -20,3 +20,30 @@ else
   best_game=$(echo $user_info | cut -d '|' -f 3)
   echo "Welcome back, $username! You have played $games_played games, and your best game took $best_game guesses."
 fi
+
+# Generación del número secreto y validación de la adivinanza
+
+secret_number=$(( RANDOM % 1000 + 1 ))
+
+echo "Guess the secret number between 1 and 1000:"
+guess=0
+tries=0
+
+while [[ $guess -ne $secret_number ]]
+do
+  read guess
+  tries=$((tries + 1))
+
+  if ! [[ $guess =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+  elif [[ $guess -gt $secret_number ]]
+  then
+    echo "It's lower than that, guess again:"
+  elif [[ $guess -lt $secret_number ]]
+  then
+    echo "It's higher than that, guess again:"
+  fi
+done
+
+echo "You guessed it in $tries tries. The secret number was $secret_number. Nice job!"
